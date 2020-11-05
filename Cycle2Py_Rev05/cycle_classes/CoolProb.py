8440d7d735974889f6b1620075db8ca6ca2532a6
aymhenry <aymhenry@gmail.com>
2020-11-05 13:08:03 +0200

    Cool_prob update

--- Cycle2Py_Rev05/cycle_classes/CoolProb.py
+++ Cycle2Py_Rev05/cycle_classes/CoolProb.py
@@ -9,6 +9,8 @@ class CoolProb:
 	# Error internal code
 	ERR_NOT_FOUND = 0	# no error code
 	ERR_FUILD_NOT_FOUND = 10   	# some error found
+	ERR_PROB_NOT_FOUND =  500   # Property not supported
+	ERR_PROB_ERROR =  510   # Property not supported
 		
 	def setup (self, strFluid ):
 		# member varbiable
@@ -45,11 +47,77 @@ class CoolProb:
 		if self.m_error == CoolProb.ERR_FUILD_NOT_FOUND :
 			return "Err " + str(CoolProb.ERR_FUILD_NOT_FOUND) + " Fuild is not supported: " + str(self.m_error_desc)
 		
+		elif self.m_error == CoolProb.ERR_PROB_ERROR :
+			return "Err " + str(CoolProb.ERR_PROB_ERROR) + " Call Prop error, error in parameters " + str(self.m_error_desc)
+			
+		elif self.m_error == CoolProb.ERR_PROB_NOT_FOUND :
+			return "Err " + str(CoolProb.ERR_PROB_NOT_FOUND) + " Property not supported: " + str(self.m_error_desc)
+			
 		elif self.m_error == CoolProb.ERR_NOT_FOUND :
 			return "No error."
 		else:
 			return "No error description, info. number CoolProb:" + str(self.m_error)
 
+	#-----------------------------------------------------------
+	# Job 			: get of of the properties 'P','T','V','V','H','S','CP','CV'
+	# Input 		: getProb code of required property, referance properties
+	#
+	# Output		: text of error number and error description
+	#-----------------------------------------------------------
+	def Property (self, getProb, P = None, T = None, V = None, D=None,\
+					H = None, S = None, Q = None ):
+		MAX_PARA = 2
+		lst_prob_io = ['P','T','V','D','H','S'] # probs for input/output
+		lst_prob_o = ['CP','CV']  # probs for output only
+		lst_prob_i = ['Q']  # probs for input only
+		
+		getProb_adj = getProb.upper()
+		
+		if getProb_adj not in lst_prob_io + lst_prob_o:
+			self.m_error = CoolProb.ERR_PROB_NOT_FOUND
+			self.m_error_desc = "Property: " + getProb
+			return
+		
+		# adjust id for Cp and Cv
+		if getProb_adj in lst_prob_o:
+			getProb_adj = getProb_adj + "MASS"
+
+		# adjust V (volume) - coolprob use D for dencity
+		if getProb == 'V':
+			getProb_adj = "D"
+			
+		#get two referance parameters
+		int_count = 0
+		
+		try:		
+			str_command = "PropsSI(" + "'" + getProb_adj  + "'"
+			for prob in lst_prob_io + lst_prob_i:
+				if eval (prob + " !=None"):
+					int_count = int_count +1
+					
+					if prob == 'V': # CoolProb use D for dencity
+						prob_code = "D"
+						prob_val = 1/eval(prob)
+					else:
+						prob_code = prob
+						prob_val = eval(prob)
+					
+					str_command = str_command + ", " + "'" + prob_code + "'" + ", " + str(prob_val)
+					
+					if int_count >=MAX_PARA: break # only limited number of parameters
+			
+			str_command = str_command + ', self.m_fluid)'
+
+			result = eval( str_command)
+			if getProb == 'V':
+				result = 1/result
+				
+			return result
+		except:
+			self.m_error = CoolProb.ERR_PROB_ERROR
+			self.m_error_desc = str_command
+			return None
+		
 	#-----------------------------------------------------------
 	# Job 			: Get Used Fuild
 	# Input 		:
@@ -89,26 +157,6 @@ class CoolProb:
 		if self.isError() : return None
 		return PropsSI(self.m_fluid, "rhocrit")
 		
-	#-----------------------------------------------------------
-	# Job 			: Get Saturated temperature
-	# Input 		: fltPressure pressure in Pascal
-	# 
-	# Output		: Saturated Temp in K
-	#-----------------------------------------------------------
-	def getSatTemp_byPress (self, flt_P_Pascal ):
-		if self.isError() : return None
-		return PropsSI("T", "P", flt_P_Pascal, "Q", 0, self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: Get Saturated pressure
-	# Input 		: flt_Temp_K temperature in K
-	# 
-	# Output		: Saturated pressure in Pa
-	#-----------------------------------------------------------
-	def getSatPress_byTemp (self, flt_Temp_K ):
-		if self.isError() : return None
-		return PropsSI("P", "T", flt_Temp_K, "Q", 0, self.m_fluid)
-
 
 	#-----------------------------------------------------------
 	# Job 			: Check if liquied or gas phase
@@ -121,189 +169,10 @@ class CoolProb:
 
 		str_phase  =  PhaseSI("P", flt_P_Pascal, "T", flt_Temp_K, self.m_fluid) 
 		#str_phase =  PropsSI("P", flt_P_Pascal, "T", flt_Temp_K, self.m_fluid)
-		return True if str_phase =='liquid' else False
+		return str_phase 
 		
-	#-----------------------------------------------------------
-	# Job 			: get Cp
-	# Input 		: fltPressure pressure in Pascal, temperature in K
-	# 
-	# Output		: Cp in J/kg/K
-	#-----------------------------------------------------------
-	def getCp_byPressTemp (self, flt_P_Pascal, flt_Temp_K ):
-		if self.isError() : return None
 
-		return PropsSI("CPMASS", "P", flt_P_Pascal, "T", flt_Temp_K, self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: get Cv
-	# Input 		: fltPressure pressure in Pascal, temperature in K
-	# 
-	# Output		: Cv in J/kg/K
-	#-----------------------------------------------------------
-	def getCv_byPressTemp (self, flt_P_Pascal, flt_Temp_K ):
-		if self.isError() : return None
-
-		return PropsSI("CVMASS", "P", flt_P_Pascal, "T", flt_Temp_K, self.m_fluid)
 		
-	#-----------------------------------------------------------
-	# Job 			: get Enthalpy
-	# Input 		: fltPressure pressure in Pascal, temperature in K
-	# 
-	# Output		: Enthalpy in J/kg
-	#-----------------------------------------------------------
-	def getH_byPressTemp (self, flt_P_Pascal, flt_Temp_K ):
-		if self.isError() : return None
-
-		return PropsSI("H", "P", flt_P_Pascal, "T", flt_Temp_K, self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: get Enthalpy
-	# Input 		: fltPressure pressure in Pascal, flt_Vol_m3kg in m3/kg
-	# 
-	# Output		: Enthalpy in J/kg
-	#-----------------------------------------------------------
-	def getH_byPressVol (self, flt_P_Pascal, flt_Vol_m3kg ):
-		if self.isError() : return None
-		if flt_Vol_m3kg ==0 : return None
-
-		return PropsSI("H", "P", flt_P_Pascal, "D", 1/flt_Vol_m3kg, self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: get Cp
-	# Input 		: fltPressure pressure in Pascal, flt_Vol_m3kg in m3/kg
-	# 
-	# Output		: Cp in J/kg K
-	#-----------------------------------------------------------
-	def getCp_byPressVol (self, flt_P_Pascal, flt_Vol_m3kg ):
-		if self.isError() : return None
-		if flt_Vol_m3kg ==0 : return None
-
-		return PropsSI("CPMASS", "P", flt_P_Pascal, "D", 1/flt_Vol_m3kg, self.m_fluid)
-		
-	#-----------------------------------------------------------
-	# Job 			: get Cv
-	# Input 		: fltPressure pressure in Pascal, flt_Vol_m3kg in m3/kg
-	# 
-	# Output		: Cv in J/kg K
-	#-----------------------------------------------------------
-	def getCv_byPressVol (self, flt_P_Pascal, flt_Vol_m3kg ):
-		if self.isError() : return None
-		if flt_Vol_m3kg ==0 : return None
-
-		return PropsSI("CVMASS", "P", flt_P_Pascal, "D", 1/flt_Vol_m3kg, self.m_fluid)
-	#-----------------------------------------------------------
-	# Job 			: get Enthalpy
-	# Input 		: flt_Temp_K temperature in K, flt_Vol_m3kg in m3/kg
-	# 
-	# Output		: Enthalpy in J/kg
-	#-----------------------------------------------------------
-	def getH_byTempVol (self, flt_Temp_K, flt_Vol_m3kg ):
-		if self.isError() : return None
-		if flt_Vol_m3kg ==0 : return None
-
-		return PropsSI("H", "T", flt_Temp_K, "D", 1/flt_Vol_m3kg, self.m_fluid)
-
-
-
-	#-----------------------------------------------------------
-	# Job 			: get Entory
-	# Input 		: fltPressure pressure in Pascal, temperature in K
-	# 
-	# Output		: Enthalpy in J/kg/K
-	#-----------------------------------------------------------
-	def getS_byPressTemp (self, flt_P_Pascal, flt_Temp_K ):
-		if self.isError() : return None
-
-		return PropsSI("S", "P", flt_P_Pascal, "T", flt_Temp_K, self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: get Entory
-	# Input 		: flt_Temp_K Temperature in K, volume in m3/kg
-	# 
-	# Output		: Enthalpy in J/kg/K
-	#-----------------------------------------------------------
-	def getS_byTempVol (self, flt_Temp_K, flt_Vol_m3kg ):
-		if self.isError() : return None
-		if flt_Vol_m3kg ==0 : return None
-				
-		return PropsSI("S", "D", 1/flt_Vol_m3kg, "T", flt_Temp_K, self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: get Temperature
-	# Input 		: Entory flt_S_jkg_K in j/kg.K, pressure flt_P_Pascal in Pa
-	# 
-	# Output		: Temperature in K
-	#-----------------------------------------------------------
-	def getT_byEntrpPress (self, flt_S_jkg_K, flt_P_Pascal ):
-		if self.isError() : return None
-				
-		return PropsSI("T", "S", flt_S_jkg_K, "P", flt_P_Pascal, self.m_fluid)
-		
-	#-----------------------------------------------------------
-	# Job 			: get Volume 
-	# Input 		: fltPressure pressure in Pascal, temperature in K
-	# 
-	# Output		: Volume in ,m3/kg
-	#-----------------------------------------------------------
-	def getV_byPressTemp (self, flt_P_Pascal, flt_Temp_K ):
-		if self.isError() : return None
-
-		return 1/self.getD_byPressTemp(flt_P_Pascal, flt_Temp_K)
-
-	#-----------------------------------------------------------
-	# Job 			: get Densisty 
-	# Input 		: fltPressure pressure in Pascal, temperature in K
-	# 
-	# Output		: Densisty in ,kg/m3
-	#-----------------------------------------------------------
-	def getD_byPressTemp (self, flt_P_Pascal, flt_Temp_K ):
-		if self.isError() : return None
-
-		return PropsSI("D", "P", flt_P_Pascal, "T", flt_Temp_K, self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: get Saturated Volume 
-	# Input 		: Temperature in K
-	# 
-	# Output		: Volume in ,m3/kg
-	#-----------------------------------------------------------
-	def getSatV_byTemp (self, flt_Temp_K, isGas = None ):
-		if self.isError() : return None
-		
-		return 1/self.getSatD_byTemp ( flt_Temp_K, isGas )
-	#-----------------------------------------------------------
-	# Job 			: get Saturated Densisty 
-	# Input 		: Temperature in K
-	# 
-	# Output		: Densisty in ,kg/m3
-	#-----------------------------------------------------------
-	def getSatD_byTemp (self, flt_Temp_K, isGas = None ):
-		if self.isError() : return None
-		inGasPhase = 1 if isGas else 0
-
-		return PropsSI("D", "T", flt_Temp_K, "Q", inGasPhase,  self.m_fluid)
-
-	#-----------------------------------------------------------
-	# Job 			: get Saturated Volume 
-	# Input 		: fltPressure pressure in Pascal
-	# 
-	# Output		: Volume in ,m3/kg
-	#-----------------------------------------------------------
-	def getSatV_byPress (self, fltPressure, isGas = None ):
-		if self.isError() : return None
-		
-		return 1/self.getSatD_byPress ( fltPressure, isGas )
-	#-----------------------------------------------------------
-	# Job 			: get Saturated Densisty 
-	# Input 		: fltPressure pressure in Pascal
-	# 
-	# Output		: Densisty in ,kg/m3
-	#-----------------------------------------------------------
-	def getSatD_byPress (self, fltPressure, isGas = None ):
-		if self.isError() : return None
-		inGasPhase = 1 if isGas else 0
-
-		return PropsSI("D", "P", fltPressure, "Q", inGasPhase,  self.m_fluid)
 #=====================
 def main ():
 	objCP = CoolProb()
@@ -325,54 +194,75 @@ def main ():
 	print ("Fuild: " , objCP.getFuild() )
 	print ("----------------------------------------")
 	print ("Critical temperature: ", objCP.getCrtiticalTemp(), "K" )
-	print ("Critical Pressure: ", objCP.getCrtiticalPress(), "Pa" )
-	print ("Critical Volume: ", objCP.getCrtiticalVolume(), "kg/m3" )
+	print ("Critical Pressure:    ", objCP.getCrtiticalPress(), "Pa" )
+	print ("Critical Volume:      ", objCP.getCrtiticalVolume(), "kg/m3" )
 	
 	print ("----------------------------------------")
-	print ("Saturated temperature for P = 1006800 Pa: (confirmed)", objCP.getSatTemp_byPress(1006800), "K" )
-	print ("Saturated Pressue for T =  315.15 Pa: (confirmed)", objCP.getSatPress_byTemp( 315.14934314624594), "Pa" )
-	print ("Phase at T=373 K, P = 1013250 Pa: (confirmed)", objCP.isLiquidPhase_byPressTemp(1013250, TK_C+100) )
-
-	print ("H (liquid) at T=273 K, P = 308150 Pa: (confirmed)", objCP.getH_byPressTemp(308150, TK_C), 'J/kg/K' )
-
-	print ("----------------------------------------")	
-	print ("Cp  at T=273 K, P = 308150 Pa: (check)", objCP.getCp_byPressTemp(308150, TK_C), 'J/kg/K' )
-	print ("Cv  at T=273 K, P = 308150 Pa: (check)", objCP.getCv_byPressTemp(308150, TK_C), 'J/kg/K' )
-
-	print ("Cp  (check)", objCP.getCp_byPressVol(P1, V), 'J/kg/K' )
-	print ("Cv  (check)", objCP.getCv_byPressVol(P1, V), 'J/kg/K' )
-	
-	print ("========================================")
-	
-	
-	print ("Given P1=", P1, "  T1=",T1, "V=", V)
-	print ("S   getS_byPressTemp ", objCP.getS_byPressTemp(P1, T1), 'J/kg/K' )
-	print ("S   getS_byTempVol   ", objCP.getS_byTempVol(T1, V), 'J/kg/K' )
-	print ("V       getV_byPressTemp ", objCP.getV_byPressTemp(P1, T1), 'm3/kg' )
-	print ("Den     getD_byPressTemp ", objCP.getD_byPressTemp(P1, T1), 'kg/m3' )
-	print ("T     getT_byEntrpPress ", objCP.getT_byEntrpPress(S, P1), 'K' )
-	
-	print ("----------------------------------------")
-	print ("H (gas) getH_byPressTemp ", objCP.getH_byPressTemp(P1, T1), 'J/kg' )
-	print ("H       getH_byPressVol  ", objCP.getH_byPressVol(P1, V), 'J/kg' )
-	print ("H       getH_byTempVol  ", objCP.getH_byTempVol(T1, V), 'J/kg' )
-		
-	print ("========================================")
 	print ("---Saturated condition")
 	T1= TK_C + 18  # K
 	P1= 535130 # Pa
 	print ("Given P1=", P1, "  T1=",T1)
-	print ("V   (liquid) getSatV_byTemp ", objCP.getSatV_byTemp(T1), 'm3/kg' )
-	print ("Den (liquid) getSatD_byTemp ", objCP.getSatD_byTemp(T1), 'kg/m3' )
-	print ("V   (gas)    getSatV_byTemp ", objCP.getSatV_byTemp(T1, True), 'm3/kg')
-	print ("Den (gas)    getSatD_byTemp ", objCP.getSatD_byTemp(T1, True), 'kg/m3')	
+	print ("----------------------------------------")
+	print ("V-liq  by T ", objCP.Property('V', T=T1, Q=0), 'kg/m3' )
+	print ("V-gas  by T ", objCP.Property('V', T=T1, Q=1), 'm3/kg' )
+	print ("D-liq  by T ", objCP.Property('D', T=T1, Q=0), 'kg/m3' )
+	print ("D-gas  by T ", objCP.Property('D', T=T1, Q=1), 'm3/kg' )	
 	
 	print ("----------------------------------------")
-	print ("V   (liquid) getSatV_byPress ", objCP.getSatV_byPress(P1), 'm3/kg' )
-	print ("Den (liquid) getSatD_byPress ", objCP.getSatD_byPress(P1), 'kg/m3' )
-	print ("V   (gas)    getSatV_byPress ", objCP.getSatV_byPress(P1, True), 'm3/kg')
-	print ("Den (gas)    getSatD_byPress ", objCP.getSatD_byPress(P1, True), 'kg/m3')	
+	print ("V-liq  by P ", objCP.Property('V', P=P1, Q=0), 'm3/kg' )
+	print ("D-liq  by P ", objCP.Property('D', P=P1, Q=0), 'kg/m3' )
+	print ("V-liq  by P ", objCP.Property('V', P=P1, Q=1), 'm3/kg' )
+	print ("D-liq  by P ", objCP.Property('D', P=P1, Q=1), 'kg/m3' )
+	
+	print ("----------------------------------------")
+	Tsat = 315.14934314624594
+	print ("Sat-Temp for P = 1006800 Pa by P Q=0", objCP.Property('T', P=1006800, Q=0), 'K' )
+	print ("Sat-Temp for P = 1006800 Pa by P Q=1", objCP.Property('T', P=1006800, Q=1), 'K' )
+
+	print ("Sat-Temp for T = 315.15 Pa by P Q=0", objCP.Property('P', T=Tsat, Q=0), 'Pa' )
+	print ("Sat-Temp for T = 315.15 Pa by P Q=1", objCP.Property('P', T=Tsat, Q=1), 'Pa' )
+	
+	print ("Phase at T=373 K, P = 1013250 Pa: (confirmed)", objCP.isLiquidPhase_byPressTemp(1013250, TK_C+100) )
+	
+	print ("========================================")
+	print ("---others=-=-=-=-=")
+	print ("H (liquid) at T=273 K, P = 308150 Pa: by P, T", objCP.Property('h', P=308150, T=TK_C), 'J/kg/K' )
+
+	print ("----------------------------------------")	
+	print ("Cp  at T=273 K, P = 308150 Pa: by P,T",objCP.Property('cp', P=308150, T=TK_C), 'J/kg/K' )
+	print ("Cp  at T=273 K, P = 308150 Pa: by P,T",objCP.Property('cv', P=308150, T=TK_C), 'J/kg/K' )
+	
+	print ("Cp  by P, V", objCP.Property('cp', P=P1, V=V), 'J/kg/K' )
+	
+	print ("Cp  by P, V", objCP.Property('cv', P=P1, V=V), 'J/kg/K' )
+
+	
+	print ("----------------------------------------")	
+	print ("Given P1=", P1, "  T1=",T1, "V=", V)
+	print ("S   by P, T ", objCP.Property("S", P=P1, T=T1), 'J/kg/K' )
+
+	print ("S   by T, V ", objCP.Property("S", T=T1, V=V), 'J/kg/K' )
+
+	
+	print ("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+	print ("V       Press,Temp ", objCP.Property("V", P=P1, T=T1), 'J/kg/K' )
+	print ("Den     Press,Temp ", objCP.Property("D", P=P1, T=T1), 'kg/m3' )
+	print ("T          by S, P ", objCP.Property("T", S=S, P=P1), 'K' )
+	
+	print ("----------------------------------------")
+	print ("H (gas) by P,T ", objCP.Property("h", P=P1, T=T1), 'J/kg' )
+	print ("H       by Press, Vol  ", objCP.Property("h", P=P1, V=V), 'J/kg' )
+	print ("H       by T, V  ", objCP.Property("h", T=T1, V=V), 'J/kg' )
+		
+	print ("========================================")
+
 #=====================
 if __name__	== '__main__':
 	main()
+	print ("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+	objCP = CoolProb()
+	objCP.setup ("R12")
+	
+	print (objCP.Property ("V", P=101325, T=300, V=.02))
+	
 	
