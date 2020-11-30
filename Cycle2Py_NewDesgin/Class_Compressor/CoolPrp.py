ca5cc5dc9b6555f66ed883a220aad62f89869e76
aymhenry <aymhenry@gmail.com>
2020-11-30 13:18:23 +0200

    Test CoolPro

--- Cycle2Py_NewDesgin/Class_Compressor/CoolPrp.py
+++ Cycle2Py_NewDesgin/Class_Compressor/CoolPrp.py
@@ -15,7 +15,7 @@ class CoolPrp:
     PHASE_TWO = "twophase"
     PHASE_LIQ = "liquid"
     PHASE_GAS = "gas"
-    
+
     def setup(self, strFluid):
         # member varbiable
         self.m_error = CoolPrp.ERR_FUILD_NOT_FOUND
@@ -81,7 +81,7 @@ class CoolPrp:
 
         # adjust X (quality) - CoolPrp use Q for quality
         Q = X
-            
+
         getProp_adj = getProp.upper()
 
         # Check that input property in good, else raise an error
@@ -122,11 +122,11 @@ class CoolPrp:
 
             str_command = str_command + ', self.m_fluid)'
             # print ("str_command: ",str_command)
-            
+
             result = eval(str_command)
             if getProp == 'V':
                 result = 1 / result
-            
+
             return result
         except BaseException:
             self.m_error = CoolPrp.ERR_PROB_ERROR
@@ -195,132 +195,20 @@ class CoolPrp:
     #
     # Output		: True or false
     # -----------------------------------------------------------
-    def is_gas_phase (self, strPhase):
+    def is_gas_phase(self, strPhase):
         if CoolPrp.PHASE_GAS in (strPhase):
             return True
         else:
             return False
 
-    def is_liquid_phase (self, strPhase):
+    def is_liquid_phase(self, strPhase):
         if CoolPrp.PHASE_LIQ in (strPhase):
             return True
         else:
             return False
 
-    def is_two_phase (self, strPhase):
+    def is_two_phase(self, strPhase):
         if CoolPrp.PHASE_TWO in (strPhase):
             return True
         else:
-            return False            
-# =====================
-def main():
-    objCP = CoolPrp()
-    objCP.setup("R12")
-
-    if objCP.isError():
-        print(objCP.err_description())
-        return
-
-    TK_C = 273.15  # K
-    T1 = TK_C + 72  # K
-    P1 = 200000  # Pa
-    # H = 400061.6198132278	# J/kg
-    S = 1741.3033288796132  # J/kg/K
-    # D = 8.632966162031755  #kg/m3
-    V = 0.11583504223589493  # m3/kg
-
-    print("Fuild: ", objCP.getFuild())
-    print("----------------------------------------")
-    print("Critical temperature: ", objCP.getCrtiticalTemp(), "K")
-    print("Critical Pressure:    ", objCP.getCrtiticalPress(), "Pa")
-    print("Critical Volume:      ", objCP.getCrtiticalVolume(), "kg/m3")
-
-    print("----------------------------------------")
-    print("---Saturated condition")
-    T1 = TK_C + 18  # K
-    P1 = 535130  # Pa
-    print("Given P1=", P1, "  T1=", T1)
-    print("----------------------------------------")
-    print("V-liq  by T ", objCP.Property('V', T=T1, X=0), 'kg/m3')
-    print("V-gas  by T ", objCP.Property('V', T=T1, X=1), 'm3/kg')
-    print("D-liq  by T ", objCP.Property('D', T=T1, X=0), 'kg/m3')
-    print("D-gas  by T ", objCP.Property('D', T=T1, X=1), 'm3/kg')
-
-    print("----------------------------------------")
-    print("V-liq  by P ", objCP.Property('V', P=P1, X=0), 'm3/kg')
-    print("D-liq  by P ", objCP.Property('D', P=P1, X=0), 'kg/m3')
-    print("V-liq  by P ", objCP.Property('V', P=P1, X=1), 'm3/kg')
-    print("D-liq  by P ", objCP.Property('D', P=P1, X=1), 'kg/m3')
-
-    print("----------------------------------------")
-    Tsat = 315.14934314624594
-    print("Sat-Temp for P = 1006800 Pa by P X=0",
-          objCP.Property('T', P=1006800, X=0), 'K')
-    print("Sat-Temp for P = 1006800 Pa by P X=1",
-          objCP.Property('T', P=1006800, X=1), 'K')
-
-    print("Sat-Pressure for T = 315.15 Pa by P X=0",
-          objCP.Property('P', T=Tsat, X=0), 'Pa')
-    print("Sat-Pressure for T = 315.15 Pa by P X=1",
-          objCP.Property('P', T=Tsat, X=1), 'Pa')
-
-    print("Phase at T=373 K, P = 1013250 Pa: (confirmed)",
-          objCP.phase_byPressTemp(1013250, TK_C + 100))
-
-    print("========================================")
-    print("---others=-=-=-=-=")
-    print("H (liquid) at T=273 K, P = 308150 Pa: by P, T",
-          objCP.Property('h', P=308150, T=TK_C), 'J/kg/K')
-
-    print("----------------------------------------")
-    print(
-        "Cp  at T=273 K, P = 308150 Pa: by P,T",
-        objCP.Property(
-            'cp',
-            P=308150,
-            T=TK_C),
-        'J/kg/K')
-    print(
-        "Cp  at T=273 K, P = 308150 Pa: by P,T",
-        objCP.Property(
-            'cv',
-            P=308150,
-            T=TK_C),
-        'J/kg/K')
-
-    print("Cp  by P, V", objCP.Property('cp', P=P1, V=V), 'J/kg/K')
-
-    print("Cp  by P, V", objCP.Property('cv', P=P1, V=V), 'J/kg/K')
-
-    print("----------------------------------------")
-    print("Given P1=", P1, "  T1=", T1, "V=", V)
-    print("S   by P, T ", objCP.Property("S", P=P1, T=T1), 'J/kg/K')
-
-    print("S   by T, V ", objCP.Property("S", T=T1, V=V), 'J/kg/K')
-
-    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-    print("V       Press,Temp ", objCP.Property("V", P=P1, T=T1), 'J/kg/K')
-    print("Den     Press,Temp ", objCP.Property("D", P=P1, T=T1), 'kg/m3')
-    print("T          by S, P ", objCP.Property("T", S=S, P=P1), 'K')
-
-    print("----------------------------------------")
-    print("H (gas) by P,T ", objCP.Property("h", P=P1, T=T1), 'J/kg')
-    print("H       by Press, Vol  ", objCP.Property("h", P=P1, V=V), 'J/kg')
-    print("H       by T, V  ", objCP.Property("h", T=T1, V=V), 'J/kg')
-
-    print("========================================")
-    print ("T1 = ", T1)
-    print("H       by T, X  ", objCP.Property("h", T=T1, X=0), 'J/kg')
-    if objCP.isError():
-        print ("Error: " + objCP.err_description())
-
-    print("========================================")
-    print ("Output ->",objCP.phase_byPressTemp (1013250, 273+100))
-    print ("Is Gas ->",objCP.is_gas_phase(objCP.phase_byPressTemp (1013250, 273+100)))
-
-    print("========================================")
-    print ("Ouput  ->",objCP.phase_byPressTemp (1013250, 273+00 ))
-    print ("Is liquid ->", objCP.is_liquid_phase(objCP.phase_byPressTemp (1013250, 273+00)))
-# =====================
-if __name__ == '__main__':
-    main()
+            return False
---
+++ Cycle2Py_NewDesgin/Class_Compressor/Test_CoolPrp.py
@@ -0,0 +1,115 @@
+# Python Import ==================
+
+# User Import ======================
+from CoolPrp import *
+
+#================== Test Driver ========
+
+objCP = CoolPrp()
+objCP.setup("R12") # used Fuild RF12
+
+if objCP.isError():
+    print(objCP.err_description())
+    exit
+
+TK_C = 273.15  # K
+T1 = TK_C + 72  # K
+P1 = 200000  # Pa
+# H = 400061.6198132278	# J/kg
+S = 1741.3033288796132  # J/kg/K
+# D = 8.632966162031755  #kg/m3
+V = 0.11583504223589493  # m3/kg
+
+print("Fuild: ", objCP.getFuild())
+print("----------------------------------------")
+print("Critical temperature: ", objCP.getCrtiticalTemp(), "K")
+print("Critical Pressure:    ", objCP.getCrtiticalPress(), "Pa")
+print("Critical Volume:      ", objCP.getCrtiticalVolume(), "kg/m3")
+
+print("----------------------------------------")
+print("---Saturated condition")
+T1 = TK_C + 18  # K
+P1 = 535130  # Pa
+print("Given P1=", P1, "  T1=", T1)
+print("----------------------------------------")
+print("V-liq  by T ", objCP.Property('V', T=T1, X=0), 'kg/m3')
+print("V-gas  by T ", objCP.Property('V', T=T1, X=1), 'm3/kg')
+print("D-liq  by T ", objCP.Property('D', T=T1, X=0), 'kg/m3')
+print("D-gas  by T ", objCP.Property('D', T=T1, X=1), 'm3/kg')
+
+print("----------------------------------------")
+print("V-liq  by P ", objCP.Property('V', P=P1, X=0), 'm3/kg')
+print("D-liq  by P ", objCP.Property('D', P=P1, X=0), 'kg/m3')
+print("V-liq  by P ", objCP.Property('V', P=P1, X=1), 'm3/kg')
+print("D-liq  by P ", objCP.Property('D', P=P1, X=1), 'kg/m3')
+
+print("----------------------------------------")
+Tsat = 315.14934314624594
+print("Sat-Temp for P = 1006800 Pa by P X=0",
+      objCP.Property('T', P=1006800, X=0), 'K')
+print("Sat-Temp for P = 1006800 Pa by P X=1",
+      objCP.Property('T', P=1006800, X=1), 'K')
+
+print("Sat-Pressure for T = 315.15 Pa by P X=0",
+      objCP.Property('P', T=Tsat, X=0), 'Pa')
+print("Sat-Pressure for T = 315.15 Pa by P X=1",
+      objCP.Property('P', T=Tsat, X=1), 'Pa')
+
+print("Phase at T=373 K, P = 1013250 Pa: (confirmed)",
+      objCP.phase_byPressTemp(1013250, TK_C + 100))
+
+print("========================================")
+print("---others=-=-=-=-=")
+print("H (liquid) at T=273 K, P = 308150 Pa: by P, T",
+      objCP.Property('h', P=308150, T=TK_C), 'J/kg/K')
+
+print("----------------------------------------")
+print(
+    "Cp  at T=273 K, P = 308150 Pa: by P,T",
+    objCP.Property(
+        'cp',
+        P=308150,
+        T=TK_C),
+    'J/kg/K')
+print(
+    "Cp  at T=273 K, P = 308150 Pa: by P,T",
+    objCP.Property(
+        'cv',
+        P=308150,
+        T=TK_C),
+    'J/kg/K')
+
+print("Cp  by P, V", objCP.Property('cp', P=P1, V=V), 'J/kg/K')
+
+print("Cp  by P, V", objCP.Property('cv', P=P1, V=V), 'J/kg/K')
+
+print("----------------------------------------")
+print("Given P1=", P1, "  T1=", T1, "V=", V)
+print("S   by P, T ", objCP.Property("S", P=P1, T=T1), 'J/kg/K')
+
+print("S   by T, V ", objCP.Property("S", T=T1, V=V), 'J/kg/K')
+
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("V       Press,Temp ", objCP.Property("V", P=P1, T=T1), 'J/kg/K')
+print("Den     Press,Temp ", objCP.Property("D", P=P1, T=T1), 'kg/m3')
+print("T          by S, P ", objCP.Property("T", S=S, P=P1), 'K')
+
+print("----------------------------------------")
+print("H (gas) by P,T ", objCP.Property("h", P=P1, T=T1), 'J/kg')
+print("H       by Press, Vol  ", objCP.Property("h", P=P1, V=V), 'J/kg')
+print("H       by T, V  ", objCP.Property("h", T=T1, V=V), 'J/kg')
+
+print("========================================")
+print ("T1 = ", T1)
+print("H       by T, X  ", objCP.Property("h", T=T1, X=0), 'J/kg')
+if objCP.isError():
+    print ("Error: " + objCP.err_description())
+
+print("========================================")
+print ("Output ->",objCP.phase_byPressTemp (1013250, 273+100))
+print ("Is Gas ->",objCP.is_gas_phase(objCP.phase_byPressTemp (1013250, 273+100)))
+
+print("========================================")
+print ("Ouput  ->",objCP.phase_byPressTemp (1013250, 273+00 ))
+print ("Is liquid ->", objCP.is_liquid_phase(objCP.phase_byPressTemp (1013250, 273+00)))
+    
